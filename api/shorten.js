00413ee60da27ccde0065c0f901e4deb5ab91226
krishnamishra-tech <krishnamishrayt65@gmail.com>
2026-08-17 08:39:38 +0530

    Update shorten.js

--- api/shorten.js
+++ api/shorten.js
@@ -19,7 +19,7 @@ async function tryVGd(longUrl) {
   throw new Error(data.errormessage || 'v.gd failed');
 }
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
